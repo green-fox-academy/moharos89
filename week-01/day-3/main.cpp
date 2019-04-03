c398e87f0ec32d720bcba0e16e43eddd443154ed
Bence Moharos <moharos89@gmail.com>
2019-04-03 21:42:48 +0200

    1. task mybe

---
+++ week-01/day-3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello, Bence! \n" << std::endl;
+
+    std::cout << "Humpty Dumpty sat on a wall," << std::endl;
+    std::cout << "Humpty Dumpty had a great fall." << std::endl;
+    std::cout << "All the king's horses and all the king's men" << std::endl;
+    std::cout << "Couldn't put Humpty together again. \n" << std::endl;
+
+    std::cout << "Hello, Jonas" << std::endl;
+    std::cout << "Hello, Sanyi" << std::endl;
+    std::cout << "Hello, Beno \n" << std::endl;
+
+    std::string a = "Hello, World!";
+    std::cout << a << std::endl;
+    int b = 42;
+    std::cout << b << std::endl;
+    float c = 3.1415f;
+    std::cout << c << std::endl;
+
+    return 0;
+}
---
+++ week-01/day-3/wontcheat/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int a = 0;
+    while (a <100) {
+        std::cout << "I won't cheat on the exam " << a << std::endl;
+        a += 1;
+    }
+
+
+
+    return 0;
+}
