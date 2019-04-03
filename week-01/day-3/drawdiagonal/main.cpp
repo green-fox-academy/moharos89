cc893a0e9b84ac1e750139c35f054e3577e72e0c
Bence Moharos <moharos89@gmail.com>
2019-04-03 21:37:34 +0200

    drawdiagonal

---
+++ week-01/day-3/drawdiagonal/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    int a ;
+    std::cout << " Do you want to draw a diagonal, pleas write down a number!" << std::endl;
+    std::cin >> a;
+    for (int b = 1 ; b <= a; b++) {
+        if (b == 1) {
+
+            std::cout <<
+        }
+        std::cout <<
+    }
+
+
+
+
+
+    return 0;
+}
