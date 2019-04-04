2e6e65da5f0cb8fa05e408681f37329adb076dc3
Bence Moharos <moharos89@gmail.com>
2019-04-04 16:46:54 +0200

    change element

---
+++ week-01/day-4/change element/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+int a [6] {1 , 2 , 3 , 8 , 5 , 6};
+a [3] = 4;
+std::cout << a [3] << std::endl;
+    // - Create an array variable named `s`
+    //   with the following content: `[1, 2, 3, 8, 5, 6]`
+    // - Change the 8 to 4
+    // - Print the fourth element
+
+    return 0;
+}
