f57ee8b8f2979b1077fac80be4929bca42e51c5b
Bence Moharos <moharos89@gmail.com>
2019-04-04 16:54:23 +0200

    third

---
+++ week-01/day-4/third/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *args[]) {
+    int q[4] = {4, 5, 6, 7};
+    std::cout << q[2] << std::endl;
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+
+    return 0;
+}
