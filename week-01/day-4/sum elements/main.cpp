dccf4dcb5399040d1608b72b73e7d44bb583453a
Bence Moharos <moharos89@gmail.com>
2019-04-04 16:52:09 +0200

    sum elements

---
+++ week-01/day-4/sum elements/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int r [4]{54 , 23 , 66 , 12};
+    std::cout<< r [2] + r [3] << std::endl;
+    // - Create an array variable named `r`
+    //   with the following content: `[54, 23, 66, 12]`
+    // - Print the sum of the second and the third element
+
+    return 0;
+}
