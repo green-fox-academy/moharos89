4214eca9b5001006b47a81a893a428e66313e26a
Bence Moharos <moharos89@gmail.com>
2019-04-07 16:17:32 +0200

    factorial

--- week-01/day-4/factorial/main.cpp
+++ week-01/day-4/factorial/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <string>
 
-void factorio (int&);
+void factorio (int , int&);
 
 int main(int argc, char* args[]) {
     int b;
     std::cout << "I want a number!" << std::endl;
     std::cin >> b;
 
-    int a = b;
+    int a;
 
-    factorio (a);
+    factorio (b , a);
 
     std::cout << a << std::endl;
 
@@ -21,9 +21,9 @@ int main(int argc, char* args[]) {
     return 0;
 }
 
-void factorio (int& b) {
+void factorio (int a , int& b) {
     int f = 1;
-    for (int d = 0; d <= b; d++) {
+    for (int d = 0; d <= a; d++) {
         if (d != 0){
             f *= d;
         }
