c6148ac4a88ab6b47be1e86e8573991f405d0482
Bence Moharos <moharos89@gmail.com>
2019-04-08 16:13:47 +0200

    refactor

---
+++ week-02/day-1/refactor/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // Refactor time!
+    // use your 'refactor_maximum.cpp', solve the same exercise by creating a separate function
+    // for each functionality
+
+    return 0;
+}
