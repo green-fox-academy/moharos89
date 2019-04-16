6bbf49001ef4c3d29ad9a59797bebe101b579c29
Bence Moharos <moharos89@gmail.com>
2019-04-16 17:20:49 +0200

    stringsagain again it is not finished

---
+++ week-02/day-5/stringsagainagain/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+void starput(std::string);
+
+int main() {
+
+    std::string proba = "Sleep";
+
+    std::cout << starput(proba) << std::endl;
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+void starput(std::string proba) {
+
+}
