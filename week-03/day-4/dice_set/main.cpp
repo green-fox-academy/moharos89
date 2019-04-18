36262a0fd0e628517a112b84497f3c50ade210df
Bence Moharos <moharos89@gmail.com>
2019-04-18 17:29:10 +0200

    dice_set

---
+++ week-03/day-4/dice_set/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "dice_set.h"
+
+int main(int argc, char* args[])
+{
+    // You have a `DiceSet` class which has a list for 6 dices
+    // You can roll all of them with roll()
+    // Check the current rolled numbers with getCurrent()
+    // You can reroll with roll()
+    // Your task is to roll the dices until all of the dices are 6
+    DiceSet diceSet;
+    diceSet.roll();
+    for(int i = 0; i < 6; i++) {
+        while (diceSet.getCurrent(i) != 6){
+            diceSet.roll(i);
+        }
+    }
+   for(int i = 0; i < 6; i++) {
+       std::cout << diceSet.getCurrent()[i] << std::endl;
+   }
+
+    return 0;
+}
