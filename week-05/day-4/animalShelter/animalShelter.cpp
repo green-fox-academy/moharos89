a28bfce1369b941ed9bf9c472519bd8eba1250e5
Bence Moharos <moharos89@gmail.com>
2019-05-02 16:55:27 +0200

    project (animal shelter) added class animal shelter

---
+++ week-05/day-4/animalShelter/animalShelter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bence on 2019.05.02..
+//
+
+#include "animalShelter.h"
---
+++ week-05/day-4/animalShelter/animalShelter.h
@@ -0,0 +1,19 @@
+//
+// Created by Bence on 2019.05.02..
+//
+
+#ifndef ANIMALSHELTER_ANIMALSHELTER_H
+#define ANIMALSHELTER_ANIMALSHELTER_H
+
+#include "animal.h"
+#include <vector>
+
+class animalShelter {
+public:
+
+private:
+    
+};
+
+
+#endif //ANIMALSHELTER_ANIMALSHELTER_H
