0ee4a6b1d3e4855b925a0779e602444dafa49f05
Bence Moharos <moharos89@gmail.com>
2019-05-02 17:16:32 +0200

    project (animal shelter) add new method to animal shelter class

--- week-05/day-4/animalShelter/animalShelter.cpp
+++ week-05/day-4/animalShelter/animalShelter.cpp
@@ -38,3 +38,43 @@ void AnimalShelter::setAdopters(const std::vector<std::string> &adopters)
 {
     _adopters = adopters;
 }
+
+int AnimalShelter::rescue(Animal *animal)
+{
+    _animals.push_back(animal);
+}
+
+int AnimalShelter::heal()
+{
+    for (int i = 0; i < _animals.size(); ++i) {
+        if (!_animals[i]->isIsHealthy() && _budget >= _animals[i]->getHealCost()) {
+            _animals[i]->setIsHealthy(true);
+            break;
+        }
+    }
+}
+
+void AnimalShelter::addAdopter(std::string name)
+{
+    _adopters.push_back(name);
+
+}
+
+void AnimalShelter::findNewOwner()
+{
+
+}
+
+int AnimalShelter::earnDonation(int amount)
+{
+    int newBudget = _budget + amount;
+    setBudget(newBudget);
+    return getBudget();
+}
+
+void AnimalShelter::toString()
+{
+    std::cout << "Budget: " << getBudget() << "E,\n" << "There are " << _animals.size() << " animal(s) and "
+              << _adopters.size() << " potentional adopter(s)" << std::endl;
+
+}
