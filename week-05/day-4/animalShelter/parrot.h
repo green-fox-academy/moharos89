cd89785d323b4f3bec870e5a9bd96716ab0c7235
Bence Moharos <moharos89@gmail.com>
2019-05-02 16:50:26 +0200

    project (animal shelter) added class parrot

---
+++ week-05/day-4/animalShelter/parrot.h
@@ -0,0 +1,18 @@
+//
+// Created by Bence on 2019.05.02..
+//
+
+#ifndef ANIMALSHELTER_PARROT_H
+#define ANIMALSHELTER_PARROT_H
+
+
+#include "animal.h"
+
+class Parrot : public Animal {
+public:
+
+private:
+};
+
+
+#endif //ANIMALSHELTER_PARROT_H
