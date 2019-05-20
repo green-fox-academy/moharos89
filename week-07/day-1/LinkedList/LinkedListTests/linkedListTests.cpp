a934b15457c4cbd73bfec45e4392a5e9703e6602
Bence Moharos <moharos89@gmail.com>
2019-05-20 17:28:25 +0200

    projet (LinkedList) first test added and done

--- week-07/day-1/LinkedList/LinkedListTests/linkedListTests.cpp
+++ week-07/day-1/LinkedList/LinkedListTests/linkedListTests.cpp
@@ -3,9 +3,24 @@
 //
 
 #include "gtest/gtest.h"
+
 extern "C" {
 #include "linked_list.h"
 }
 
-
-
+TEST(linkedList, TestAppendOneNewNode)
+{
+    //Arrange
+    linked_list_t *list = (linked_list_t *) malloc(sizeof(linked_list_t));
+    list->data = 2;
+    list->next =NULL;
+    //Act
+    append(list , 3);
+    int result = list->next->data;
+    //Assert
+    ASSERT_EQ(result , 3);
+    free(list->next);
+    list->next = NULL;
+    free(list);
+    list = NULL;
+}
