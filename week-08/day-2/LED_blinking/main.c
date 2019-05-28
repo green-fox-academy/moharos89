5ad767a43ef572374d44099178282a7229da5575
Bence Moharos <moharos89@gmail.com>
2019-05-28 14:46:38 +0200

    project (LED blinking) completed

---
+++ week-08/day-2/LED_blinking/main.c
@@ -0,0 +1,118 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+static void SystemClock_Config(void);
+void led_init();
+void timer_init();
+
+TIM_HandleTypeDef base_timer;
+
+int main(void)
+{
+	HAL_Init();
+	SystemClock_Config();
+
+	led_init();
+	timer_init();
+
+	/* starting the timer */
+	HAL_TIM_Base_Start(&base_timer);
+	uint16_t timer_value;
+
+	while (1) {
+		timer_value = __HAL_TIM_GET_COUNTER(&base_timer);
+		if (timer_value == 0) {
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET);
+		}
+		if (timer_value == 1000) {
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET);
+		}
+	}
+	return 0;
+}
+
+void led_init()
+{
+	__HAL_RCC_GPIOF_CLK_ENABLE()
+	;
+
+	GPIO_InitTypeDef gpio_led;
+
+	gpio_led.Pin = GPIO_PIN_10;
+	gpio_led.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio_led.Pull = GPIO_NOPULL;
+	gpio_led.Speed = GPIO_SPEED_FREQ_HIGH;
+
+	HAL_GPIO_Init(GPIOF, &gpio_led);
+}
+
+void timer_init()
+{
+	__HAL_RCC_TIM2_CLK_ENABLE()
+	;
+
+	base_timer.Instance = TIM2;
+	base_timer.Init.Prescaler = 54000 - 1;
+	base_timer.Init.Period = 2000 - 1;
+	base_timer.Init.CounterMode = TIM_COUNTERMODE_UP;
+	base_timer.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+
+	HAL_TIM_Base_Init(&base_timer);
+}
+
+void Error_Handler(void)
+{
+}
+
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+	/**Configure the main internal regulator output voltage */
+	__HAL_RCC_PWR_CLK_ENABLE()
+	;
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	/**Initializes the CPU, AHB and APB busses clocks */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 216;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 2;
+
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/**Activate the Over-Drive mode */
+	if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+		Error_Handler();
+	}
+
+	/**Initializes the CPU, AHB and APB busses clocks */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
+		Error_Handler();
+	}
+}
